7232d7c713b3011d9071f9d113c9e3dfaeb83890
Leandro <leandro.moniz@gmail.com>
2014-11-11 02:18:14 -0200

    Fitness.runs tem a estrutura da classe Post. Pode ser usada uma classe especifica pra fitness para deixar a modelagem mais enxuta. Aprendizado para definir nos oarametros exatamente os fields necessários. Aprendizado de que existem fields que são ocultos e devem ser explicitamente requisitados no parametro fields.

--- src/main/java/br/com/truefriends/controller/RecuperaUsuarioFacebook.java
+++ src/main/java/br/com/truefriends/controller/RecuperaUsuarioFacebook.java
@@ -17,9 +17,11 @@ import br.com.truefriends.service.UsuarioServico;
 import com.restfb.Connection;
 import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
+import com.restfb.Parameter;
 import com.restfb.json.JsonArray;
 import com.restfb.json.JsonObject;
 import com.restfb.types.FacebookType;
+import com.restfb.types.Post;
 import com.restfb.types.User;
 
 
@@ -39,23 +41,41 @@ public class RecuperaUsuarioFacebook extends HttpServlet {
 	   
 	   FacebookClient facebookClient = new DefaultFacebookClient(request.getParameter("token"));
 	   User facebookUser = facebookClient.fetchObject("me", User.class);
-	   Connection<User> friendsFB = facebookClient.fetchConnection("me/friends", User.class);
+	   Connection<User> friendsFB = facebookClient.fetchConnection("me/friends", User.class, Parameter.with("fields", "name, id, picture"));
 	   
-	   //Connection<Post> me = facebookCliente.fetchConnection("me/activities", User.class);
 	   String friendsRuns = "";
-	   String friendsName = "";
+//	   String runsJson = "{\"data\":[";
+	   String runsJson = "";
+	   String[] profPicUrls = new String[friendsFB.getData().size()];
+		  
 	   for (int i=0;i < friendsFB.getData().size(); i++) {
 		  String nome = friendsFB.getData().get(i).getName();
 		  String id = friendsFB.getData().get(i).getId();
-		  String runsJson = "";
-
-		  JsonObject runsConnection = facebookClient.fetchObject(id + "/fitness.runs", JsonObject.class);
-		  if (runsConnection.getJsonArray("data").length() != 0 ) {
-			  runsJson = runsJson + runsConnection.getJsonArray("data").getJsonObject(0).getString("start_time");
-		  }
+		  profPicUrls[i] = friendsFB.getData().get(i).getPicture() != null ? friendsFB.getData().get(i).getPicture().getUrl() : "imagem/401.png" ;
+		  
+		  //JsonObject runsConnection = facebookClient.fetchObject(id + "/fitness.runs", JsonObject.class);
+		  Connection<Post> runsConnection = facebookClient.fetchConnection(id + "/fitness.runs", Post.class, Parameter.with("limit", "999"));
+		 /* if (runsConnection.getJsonArray("data").length() != 0 ) {
+			  for(int j=0;j < runsConnection.getJsonArray("data").length(); j++) {
+				  
+				  for(int k=0;k < runsConnection.getJsonArray("data").getJsonObject(j).names().length();k++) {
+					  runsJson = runsJson + runsConnection.getJsonArray("data").getJsonObject(j).names().get(k) + "<br>";
+				  }
+				  runsJson = runsJson + "-------<br>";
+//				  runsJson = runsJson + runsConnection.getJsonArray("data").getJsonObject(j).names();
+//				  if(j == (runsConnection.getJsonArray("data").length() - 1)) {
+//					  runsJson = runsJson + runsConnection.getJsonArray("data").getJsonObject(j);
+//				  } else {
+//					  runsJson = runsJson + runsConnection.getJsonArray("data").getJsonObject(j) + ",";
+//				  }
+			  }
+		  }*/
 
 		  friendsRuns = nome + " " + id + " " + "<br />";
 	   }
+//	   runsJson = runsJson + "]}";
+	   
+//	   JsonObject allFitJson = new JsonObject(runsJson);
 	   
 	   String postsString = "";
 	  
@@ -77,10 +97,12 @@ public class RecuperaUsuarioFacebook extends HttpServlet {
 //	   request.setAttribute("id_usuario", usuario.getId());
 //	   request.setAttribute("email_usuario", usuario.getEmail());
 //	   request.setAttribute("dtnasc_usuario", usuario.getDataNascimento());
-	   request.setAttribute("friendsCount", friendsFB.getData().size());
+	  /* request.setAttribute("friendsCount", friendsFB.getData().size());
 	   request.setAttribute("friendsNames", friendsName);
 	   request.setAttribute("myPosts", postsString);
-	   
+	   */
+	   request.setAttribute("profPicUrl", profPicUrls);
+	   request.setAttribute("runsJson", runsJson);
 	   
 	   RequestDispatcher rd = request.getRequestDispatcher("/results.jsp");  
 	   rd.forward(request,response);  
